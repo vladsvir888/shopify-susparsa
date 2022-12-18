5110da508bccfcb014bb0539095d10829c561d12
vsvirydovich <vladsvir888@gmail.com>
2022-12-19 00:08:58 +0300

    section header, footer and snippet breadcrumbs

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,66 @@
+// accordion
+class Accordion {
+    static selectors = {
+        accordion: '.accordion',
+        accordion_item: '.accordion__item',
+        accordion_btn: '.accordion__btn',
+        accordion_content: '.accordion__content',
+        accordion_item_active: '.accordion__item--active'
+    }
+
+    static classes = {
+        accordion_btn: 'accordion__btn',
+        accordion_item_active: 'accordion__item--active'
+    }
+
+    constructor(element, index) {
+        this.accordion = element;
+
+        this.accordion.addEventListener('click', this.onClick.bind(this));
+
+        this.items = this.accordion.querySelectorAll(Accordion.selectors.accordion_item);
+
+        if (this.items.length > index && index >= 0) {
+            this.toggle(this.items[index]);
+        }
+    }
+
+    onClick(e) {
+        const { target } = e;
+
+        if (!target.classList.contains(Accordion.classes.accordion_btn)) return;
+
+        const accordion = target.closest(Accordion.selectors.accordion);
+
+        const accordionItem = target.closest(Accordion.selectors.accordion_item);
+
+        const accordionItemActive = accordion.querySelector(Accordion.selectors.accordion_item_active);
+
+        if (accordionItemActive && accordionItemActive !== accordionItem) {
+            this.toggle(accordionItemActive);
+        }
+
+        this.toggle(accordionItem);
+    }
+
+    toggle(element) {
+        const accordionContent = element.lastElementChild;
+        const accordionBtn = element.firstElementChild;
+
+        if (!accordionContent.style.maxHeight) {
+            element.classList.add(Accordion.classes.accordion_item_active);
+            accordionBtn.setAttribute('aria-expanded', true);
+            accordionContent.style.maxHeight = `${accordionContent.scrollHeight / parseInt(window.getComputedStyle(document.body).fontSize)}rem`;
+        } else {
+            element.classList.remove(Accordion.classes.accordion_item_active);
+            accordionBtn.setAttribute('aria-expanded', false);
+            accordionContent.removeAttribute('style');
+        }
+    }
+}
+
+document.querySelectorAll(Accordion.selectors.accordion).forEach(accordion => new Accordion(accordion, 0));
+
 // show/hide burger-menu
 const burger = () => {
     const burgerBtn = document.querySelector('.burger');
@@ -19,70 +82,3 @@ const burger = () => {
 };
 
 burger();
-
-// accordion
-const SELECTORS = {
-    ACCORDION: '.accordion',
-    ACCORDION_ITEM: '.accordion__item',
-    ACCORDION_BTN: '.accordion__btn',
-    ACCORDION_CONTENT: '.accordion__content',
-    ACCORDION_ITEM_ACTIVE: '.accordion__item--active'
-};
-
-const CLASSES = {
-    ACCORDION_BTN: 'accordion__btn',
-    ACCORDION_ITEM_ACTIVE: 'accordion__item--active'
-};
-
-class Accordion {
-    constructor(selector) {
-        this.elements = document.querySelectorAll(selector);
-
-        this.elements.forEach(element => element.addEventListener('click', this.onClick.bind(this)));
-    }
-
-    onClick(e) {
-        const { target } = e;
-
-        if (!target.classList.contains(CLASSES.ACCORDION_BTN)) return;
-
-        const accordionItem = target.closest(SELECTORS.ACCORDION_ITEM);
-
-        const accordion = target.closest(SELECTORS.ACCORDION);
-
-        const accordionItemActive = accordion.querySelector(SELECTORS.ACCORDION_ITEM_ACTIVE);
-
-        if (accordionItemActive && accordionItemActive !== accordionItem) { // if there is active and it is not equal target of parent
-            this.toggle(accordionItemActive);
-        }
-
-        this.toggle(accordionItem);
-    }
-
-    toggle(element) {
-        const accordionContent = element.querySelector(SELECTORS.ACCORDION_CONTENT);
-        const accordionBtn = element.querySelector(SELECTORS.ACCORDION_BTN);
-
-        if (!accordionContent.style.maxHeight) {
-            element.classList.add(CLASSES.ACCORDION_ITEM_ACTIVE);
-            accordionBtn.setAttribute('aria-expanded', true);
-            accordionContent.style.maxHeight = `${accordionContent.scrollHeight / parseInt(window.getComputedStyle(document.body).fontSize)}rem`;
-        } else {
-            element.classList.remove(CLASSES.ACCORDION_ITEM_ACTIVE);
-            accordionBtn.setAttribute('aria-expanded', false);
-            accordionContent.removeAttribute('style');
-        }
-    }
-
-    open(element) {
-        this.toggle(element);
-    }
-}
-
-const accordion = new Accordion(SELECTORS.ACCORDION);
-
-document.querySelectorAll(SELECTORS.ACCORDION).forEach(item => {
-    const firstElementChild = item.querySelector(SELECTORS.ACCORDION_ITEM);
-
-    accordion.open(firstElementChild);
-});
