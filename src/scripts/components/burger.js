fa91b99d59b7f5286ef519f04d9ff02bf6444160
vsvirydovich <vladsvir888@gmail.com>
2023-01-02 17:52:29 +0300

    added focus-trap for burger-menu

--- src/scripts/components/burger.js
+++ src/scripts/components/burger.js
@@ -1,16 +1,26 @@
+import * as focusTrap from 'focus-trap';
+
 class Burger {
   static selectors = {
     btn: '.burger',
     menu: '.burger-menu',
+    header: '.header',
+  };
+
+  static classes = {
+    menu: 'burger-menu--active',
   };
 
   constructor() {
     this.btn = document.querySelector(Burger.selectors.btn);
     this.menu = document.querySelector(Burger.selectors.menu);
+    this.trap = focusTrap.createFocusTrap(Burger.selectors.header);
 
     if (!this.btn) return;
 
     this.btn.addEventListener('click', this.onClick.bind(this));
+
+    document.addEventListener('keydown', this.onKeyDown.bind(this));
   }
 
   onClick() {
@@ -18,10 +28,24 @@ class Burger {
       this.btn.classList.remove('burger--active');
       this.btn.setAttribute('aria-expanded', false);
       this.menu.classList.remove('burger-menu--active');
+      this.trap.deactivate();
     } else {
       this.btn.classList.add('burger--active');
       this.btn.setAttribute('aria-expanded', true);
       this.menu.classList.add('burger-menu--active');
+      this.trap.activate();
+    }
+  }
+
+  onKeyDown(event) {
+    if (
+      event.code === 'Escape' &&
+      this.menu.classList.contains(Burger.classes.menu)
+    ) {
+      this.btn.classList.remove('burger--active');
+      this.btn.setAttribute('aria-expanded', false);
+      this.menu.classList.remove('burger-menu--active');
+      this.trap.deactivate();
     }
   }
 }
